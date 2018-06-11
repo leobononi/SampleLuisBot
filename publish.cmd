dbcaafdc98625d0f61ff71e12495edde34fb17d5
Leonardo Bononi <leonardo.bononi@programmers.com.br>
2018-06-11 14:22:02 -0300

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=myfirstluisbotapp-Web-Deploy.pubxml -p:Password=bnLj0dy9N0bK5btdjsrQb2orXQJYKR8fqrb344BTkLAtQ27zT6CAESxDdCWv
+
